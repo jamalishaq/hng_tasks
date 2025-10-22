9cc6b96fdec5654abef8c3e0b0f0e7b463320a09
jamaloflagos <toyinjamal@gmail.com>
2025-10-22 18:12:27 +0100

    implement https

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+const https = require("https");
+const fs = require("fs");
 const app = require("./app");
 const dotenv = require("dotenv");
 const { createTable } = require("./repositories/string.repository");
@@ -5,6 +7,12 @@ const db = require("./connectDB");
 dotenv.config();
 
 const startServer = () => {
+  const privateKey = fs.readFileSync("./server.key");
+  const certificate = fs.readFileSync("./server.cert");
+  const credentials = { key: privateKey, cert: certificate };
+
+  const server = https.createServer(credentials, app);
+
   db.on("error", (err) => {
     console.error("Error opening database:", err.message);
     process.exit(1);
@@ -14,7 +22,7 @@ const startServer = () => {
     createTable();
 
     const PORT = process.env.PORT;
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`Server running and listening on port ${PORT}`);
     });
   });
